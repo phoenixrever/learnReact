68fe786dbf7b96da587d39e505ab93bdcd0e6f78
phoenixhell <phoenixrever@gmail.com>
2025-09-26 22:35:37 +0900

    refactor: clean up formatting and comments in count-action.js

--- 09_redux/src/redux/count-action.js
+++ 09_redux/src/redux/count-action.js
@@ -1,21 +1,18 @@
-import {DECREMENT, INCREMENT} from "./constant";
+import { DECREMENT, INCREMENT } from './constant';
 
-//普通action 返回对象{} 
-//注意返回对象的写法 data => ({type: INCREMENT, data}) === data=>{return {type: INCREMENT, data} } 不能直接写{}
-export const createIncrementAction = data => ({type: INCREMENT, data});
-export const createDecrementAction = data => ({type: DECREMENT, data});
-
+//普通action 返回对象{}
+//注意返回对象的写法 data => ({type: INCREMENT, data}) === data=>{return {type: INCREMENT, data} } 不能直接写{} 直接写{}会被当成函数体
+export const createIncrementAction = (data) => ({ type: INCREMENT, data });
+export const createDecrementAction = (data) => ({ type: DECREMENT, data });
 
 //异步action  返回函数
 //redux 加入中间件后只会执行这个函数 并不会等待他的返回值  需要我们自己再调store的dispatch方法
 export const createAsyncIncrementAction = (data, time) => {
-    //store调用dispatch（action） 发现action是函数的时候会传入 将 dispatch 函数 作为参数传进来
-    return (dispatch) => {
-        setTimeout(() => {
-            // store.dispatch(createIncrementAction(data));
-            dispatch(createIncrementAction(data));
-        }, time)
-    }
+  //store调用dispatch（action） 发现action是函数的时候会传入 将 dispatch 函数 作为参数传进来
+  return (dispatch) => {
+    setTimeout(() => {
+      // store.dispatch(createIncrementAction(data));
+      dispatch(createIncrementAction(data));
+    }, time);
+  };
 };
-
-
